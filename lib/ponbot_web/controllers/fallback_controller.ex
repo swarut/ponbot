8b70165b5dd19788675b32daa50a95750c4f406a
Warut Surapat <s.warut@gmail.com>
2019-01-20 21:35:23 +0900

    Add fallback controller.

---
+++ lib/ponbot_web/controllers/fallback_controller.ex
@@ -0,0 +1,22 @@
+defmodule PonbotWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use PonbotWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(PonbotWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(PonbotWeb.ErrorView)
+    |> render(:"404")
+  end
+end
