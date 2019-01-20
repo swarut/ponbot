72b42304a5f7b487aed892542ddff5a69fcfda84
Warut Surapat <s.warut@gmail.com>
2019-01-20 17:24:06 +0900

    Try line web hook.

--- lib/ponbot_web/controllers/page_controller.ex
+++ lib/ponbot_web/controllers/page_controller.ex
@@ -1,9 +1,17 @@
 defmodule PonbotWeb.PageController do
   use PonbotWeb, :controller
+  require IEx
 
   def index(conn, _params) do
     conn
     |> assign(:env, Application.get_env(:ponbot, :environment))
+    |> render("index.html")
+  end
+
+  def status(conn, _params) do
+    IEx.pry
+    conn
+    |> put_status(200)
     |> render("index.html")
   end
 end
