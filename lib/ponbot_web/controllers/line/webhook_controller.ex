009bf370cb15483c30af63fe51891fac25f2d0a7
Warut Surapat <s.warut@gmail.com>
2019-01-27 19:26:51 +0900

    Splitting reply message function.

--- lib/ponbot_web/controllers/line/webhook_controller.ex
+++ lib/ponbot_web/controllers/line/webhook_controller.ex
@@ -2,33 +2,17 @@ defmodule PonbotWeb.Line.WebhookController do
   use PonbotWeb, :controller
   require IEx
 
+
   def status(conn, _params) do
-    # IEx.pry
+    IEx.pry
     events = conn.params["events"]
     [first_event| _] = events
     case first_event["type"] do
       "message" ->
-
         reply_token = first_event["replyToken"]
+        access_token = "b+rw+B8C44Nbb+pb7Naf1Gvt7b42OydZDfOHlO0IBp+GP4JOZX8HtiKN6mrfjvNcNtxfnL9uzgWssp/f05zHHzJKUyZcAqYw1O9bDiHwRKgLpQJMEGqqdPjec9q682dcAeawxEmY7AZ/kW9rXXE7LwdB04t89/1O/w1cDnyilFU="
         IO.puts("GOT MESSAGE -- #{reply_token}")
-
-        message_endpoint = "https://api.line.me/v2/bot/message/reply"
-        header = [
-          {"Content-Type", "application/json"},
-          {"Authorization", "Bearer vyBv9sYAvEbd8Gie+BRYaigCw/1SdpSCcnqJ38LernVL9ASCEjyM92JsSR5bv802NtxfnL9uzgWssp/f05zHHzJKUyZcAqYw1O9bDiHwRKhJchw3rhwmuhocQ5MLKqQ+2Rh2q24Loa7FIWPIIykeFgdB04t89/1O/w1cDnyilFU="}
-        ]
-        {:ok, body} = Jason.encode(%{
-          replyToken: reply_token,
-          messages: [%{
-            type: "text",
-            text: "hello from reply"
-          }]
-        })
-        case HTTPoison.post message_endpoint, body, header do
-          {:ok, resp} -> IO.puts("SENNNTTTT #{inspect resp}")
-          {:error, err} -> IO.puts("ERRRORRRR #{inspect err}")
-        end
-
+        reply("hi yo", reply_token, access_token)
         conn
         |> put_status(200)
         |> render("index.html")
@@ -38,5 +22,24 @@ defmodule PonbotWeb.Line.WebhookController do
         |> render("index.html")
     end
 
+  end
+
+  defp reply(message, reply_token, access_token) do
+    message_endpoint = "https://api.line.me/v2/bot/message/reply"
+    header = [
+      {"Content-Type", "application/json"},
+      {"Authorization", "Bearer #{access_token}"}
+    ]
+    {:ok, body} = Jason.encode(%{
+      replyToken: reply_token,
+      messages: [%{
+        type: "text",
+        text: message
+      }]
+    })
+    case HTTPoison.post message_endpoint, body, header do
+      {:ok, resp} -> IO.puts("SENNNTTTT #{inspect resp}")
+      {:error, err} -> IO.puts("ERRRORRRR #{inspect err}")
+    end
   end
 end
