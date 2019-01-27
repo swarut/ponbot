7846977a0b3f4715dbefeb9f02f697ab01a35f9e
Warut Surapat <s.warut@gmail.com>
2019-01-27 23:14:57 +0900

    Small refactor and add 401 handling.

--- lib/ponbot_web/controllers/line/webhook_controller.ex
+++ lib/ponbot_web/controllers/line/webhook_controller.ex
@@ -2,16 +2,18 @@ defmodule PonbotWeb.Line.WebhookController do
   use PonbotWeb, :controller
   require IEx
 
+  @message_endpoint "https://api.line.me/v2/bot/message/reply"
+  @oauth_endpoint "https://api.line.me/v2/oauth/accessToken"
+
+  # https://api.line.me/v2/oauth/accessToken
 
   def status(conn, _params) do
-    IEx.pry
     events = conn.params["events"]
     [first_event| _] = events
+    reply_token = first_event["replyToken"]
+    access_token = "b+rw+B8C44Nbb+pb7Naf1Gvt7b42OydZDfOHlO0IBp+GP4JOZX8HtiKN6mrfjvNcNtxfnL9uzgWssp/f05zHHzJKUyZcAqYw1O9bDiHwRKgLpQJMEGqqdPjec9q682dcAeawxEmY7AZ/kW9rXXE7LwdB04t89/1O/w1cDnyilFU="
     case first_event["type"] do
       "message" ->
-        reply_token = first_event["replyToken"]
-        access_token = "b+rw+B8C44Nbb+pb7Naf1Gvt7b42OydZDfOHlO0IBp+GP4JOZX8HtiKN6mrfjvNcNtxfnL9uzgWssp/f05zHHzJKUyZcAqYw1O9bDiHwRKgLpQJMEGqqdPjec9q682dcAeawxEmY7AZ/kW9rXXE7LwdB04t89/1O/w1cDnyilFU="
-        IO.puts("GOT MESSAGE -- #{reply_token}")
         reply("hi yo", reply_token, access_token)
         conn
         |> put_status(200)
@@ -25,7 +27,6 @@ defmodule PonbotWeb.Line.WebhookController do
   end
 
   defp reply(message, reply_token, access_token) do
-    message_endpoint = "https://api.line.me/v2/bot/message/reply"
     header = [
       {"Content-Type", "application/json"},
       {"Authorization", "Bearer #{access_token}"}
@@ -37,8 +38,10 @@ defmodule PonbotWeb.Line.WebhookController do
         text: message
       }]
     })
-    case HTTPoison.post message_endpoint, body, header do
-      {:ok, resp} -> IO.puts("SENNNTTTT #{inspect resp}")
+    IEx.pry
+    case HTTPoison.post @message_endpoint, body, header do
+      {:ok, resp = %HTTPoison.Response{status_code: 401}} -> IO.puts("STATUS 401: Unauthorized --- #{inspect resp}")
+      {:ok, resp = %HTTPoison.Response{status_code: 200}} -> IO.puts("SENNNTTTT #{inspect resp}")
       {:error, err} -> IO.puts("ERRRORRRR #{inspect err}")
     end
   end
