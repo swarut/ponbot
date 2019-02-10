5b8893d47a9e46381886384d0209ec4ec82da05a
Warut Surapat <s.warut@gmail.com>
2019-02-10 11:28:09 +0900

    Add follow/unfollow handler.

--- lib/ponbot_web/controllers/line/webhook_controller.ex
+++ lib/ponbot_web/controllers/line/webhook_controller.ex
@@ -13,28 +13,35 @@ defmodule PonbotWeb.Line.WebhookController do
     events = conn.params["events"]
     [first_event| _] = events
 
-    handle_message(conn, first_event, first_event["type"])
+    handle_webhook(first_event, first_event["type"])
 
     conn
     |> put_status(200)
     |> render("index.html")
   end
 
-  def handle_message(conn, event, event_type) when event_type == "message" do
+  def handle_webhook(event, event_type) when event_type == "message" do
     reply_token = event["replyToken"]
     access_token = get_access_token()
     case ExLineWrapper.reply("hi yo", reply_token, access_token) do
-      {:ok} ->
-        IO.puts "Reply message is successful sent."
-      {:error} ->
-        IO.puts "Access token is invalid"
-        {:ok, resp} = ExLineWrapper.authenticate(@channel_id, @channel_secret)
-        ExLineWrapper.reply("hi yo", reply_token, resp["access_token"])
+      {:ok} -> IO.puts "Reply message is successful sent."
     end
   end
 
+  def handle_webhook(event, event_type) when event_type == "follow" do
+    reply_token = event["replyToken"]
+    access_token = get_access_token()
+    case ExLineWrapper.reply("Hello, nice to meet you!", reply_token, access_token) do
+      {:ok} -> IO.puts "Reply message is successful sent."
+    end
+  end
+
+  def handle_webhook(_event, event_type) when event_type == "unfollow" do
+    IO.puts "Bot got unfollowed T_T"
+  end
+
   defp get_access_token do
-    access_token = case Configurations.get_setting_by_key("line_access_token") do
+    case Configurations.get_setting_by_key("line_access_token") do
       %Configurations.Setting{key: "line_access_token", value: value} -> value
       nil ->
         {:ok, resp} = ExLineWrapper.authenticate(@channel_id, @channel_secret)
