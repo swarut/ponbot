0ba776474f3f841f5b6bf1db8a0defc803e96a04
Warut Surapat <wsurapat@gilt.jp>
2019-04-02 19:00:20 +0900

    Use css import.

--- assets/js/components/chat_app.js
+++ assets/js/components/chat_app.js
@@ -7,6 +7,7 @@ let store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+// import '../../css/chat.css';
 
 class ChatApp extends Component {
   constructor(props) {
