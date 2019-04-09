dad93cbf9f9a895df998c4b6dea1598e69a1a4c8
Warut Surapat <s.warut@gmail.com>
2019-04-10 02:09:57 +0900

    Example of how process alive.

--- lib/ponbot/line_webhook/line_webhook_supervisor.ex
+++ lib/ponbot/line_webhook/line_webhook_supervisor.ex
@@ -1,6 +1,9 @@
 defmodule Ponbot.LineWebhookSupervisor do
   def handle_webhook(event, event_type) do
     opts = [restart: :transient]
-    Task.Supervisor.start_child(__MODULE__, Ponbot.LineWebhook, :handle_webhook, [event, event_type], opts )
+    p = Task.Supervisor.start_child(__MODULE__, Ponbot.LineWebhook, :handle_webhook, [event, event_type], opts )
+    child_pids = Task.Supervisor.children(__MODULE__)
+    IO.puts("p - #{inspect p}")
+    IO.puts("child count = #{length(child_pids)} --- #{inspect __MODULE__}")
   end
 end
